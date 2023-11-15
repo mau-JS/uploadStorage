faee21921db3058301e378d1e0efe4f87fc7a5b6
Mauricio Juárez <iconoclastsym@gmail.com>
2023-11-15 17:30:02 -0600

    now properly uploads the format

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ async function saveToBucket(bucketName, url, destinationBlobName) {
 
   // Uploads a local file to the bucket
   await new Promise((resolve, reject) =>
-    file.createWriteStream().on('error', reject).on('finish', resolve).end(reader.read())
+    reader.pipe(file.createWriteStream()).on('error', reject).on('finish', resolve)
   );
 }
 
 const bucketName = 'mybucket124';  // TODO: replace with your bucket name
-const url = 'https://tomato-cherida-91.tiiny.site';  // TODO: replace with the URL of the file you want to download
+const url = 'https://drive.google.com/uc?export=download&id=1Hrwtmvb_fziWFEJ9iwfV_81mh0zvuMnf';  // TODO: replace with the URL of the file you want to download
 const destinationBlobName = 'test.pdf';  // TODO: replace with the name you want to give to the object in your bucket
 
 saveToBucket(bucketName, url, destinationBlobName).catch(console.error);
